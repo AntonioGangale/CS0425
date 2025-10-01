33905fa14e8c25377cdf9c06d4664db7ab865d44
AntonioGangale <antoniogangale7@gmail.com>
2025-10-01 16:06:43 +0200

    Add files via upload

---
+++ UNIT_1/S2/L3/S2L3Consegna.py
@@ -0,0 +1,8 @@
+
+
+city = input('Inserisci il nome della tua città d\'origine: ')
+
+pet = input('Inserisci il nome del tuo animmale domestico: ')
+
+nome_band = (city + '' + pet)
+print(nome_band)
